2ba422addb74a92fe01fcf4090119ed22f25f4f3
saeednkz <saeed.vali87@gmail.com>
2025-09-03 10:05:08 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// --- وارد کردن ماژول‌ها  - --
+// --- وارد کردن ماژول‌ها - --
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { onAuthStateChanged, updatePassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import {
